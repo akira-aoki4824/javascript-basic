c92272e64e8e7e1522e96bd0df389a3f65257f67
akira.aoki4824 <sanei.6788@icloud.com>
2023-07-30 19:06:28 +0900

    benkyou

---
+++ js/class.js
@@ -0,0 +1,39 @@
+// クラスを定義する
+class Product {
+  // インスタンス化すると同時に処理を実行（初期化）する
+  constructor(name,price,category) {
+    // console.log('敏感肌にも優しい100％店天然由来のシャンプーです。');
+    // インスタンス（オブジェクト）にプロパティを持たせる
+    this.name = name;
+    this.price = price;
+    this.category = category;
+  }
+
+  // メソッドを定義する
+  describe() {
+    console.log('この商品は' + this.name + 'です。');
+  }
+}
+
+// インスタンス化する
+const shampoo = new Product('シャンプー', 500, '生活雑貨');
+const coffee = new Product('コーヒー', 1500, '飲料');
+
+// インスタンス（オブジェクト）の値を出力する
+console.log(shampoo);
+console.log(coffee);
+
+// メソッドを呼び出す（実行する）
+shampoo.describe();
+
+const user = {
+  name: '侍太郎',
+  age: 36,
+  gender: '男性',
+  greet: () => {
+    console.log('宜しくお願いします！');
+  }
+}
+
+// メソッドを呼び出す（実行する）
+user.greet();
--- js/function.js
+++ js/function.js
@@ -31,4 +31,25 @@ const addTwoArgrments = (price, shippingFee) => {
 addTwoArgrments(1200, 500);
 
 // 与えられる引数numを2倍にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+  return num * 2;
+}
+console.log(double(30));
 
+// 定数を宣言する
+// const userName = '侍太郎';
+
+// 定数の値を出力する
+// console.log(userName);
+
+// 関数の中で定数を宣言し、関数の中で定数を使う
+const useVariable = () => {
+  const userName = '侍太郎'
+  console.log(userName);
+}
+
+// 関数を呼び出す
+useVariable();
+
+// 関数の中で宣言した定数を関数の外で使う
+console.log(userName);
